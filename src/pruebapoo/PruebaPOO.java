fe4b16f156fcca97f27dd332730da143324f7236
Benjamin Franklin Velita Zorrilla <fvelita@gmail.com>
2012-07-25 01:36:08 -0500

    Formulario inicial.

---
+++ src/pruebapoo/PruebaPOO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebapoo;
+
+/**
+ *
+ * @author FRANKLIN
+ */
+public class PruebaPOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
